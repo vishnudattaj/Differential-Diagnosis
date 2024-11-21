1e932f57813f6337b2fa30bbf88d02dab496b7a6
Zong Rui Lee <zrlee71508@gmail.com>
2024-11-21 03:21:30 -0600

    Added checkbox for for user to have to accept terms and conditions so we can sell their data Added php filter improved database readibility Added verification to make sure user selects they must accept the conditions fixed some bugs

--- homepage/processform.php
+++ homepage/processform.php
@@ -1,3 +1,13 @@
 <?php
 
-print_r($_POST); 
+$symptoms = $_POST["symptoms"];
+$diabetes = $_POST["diabetes"];
+$hypertension = $_POST["hypertension"];
+$asthma = $_POST["asthma"];
+$terms = filter_input(INPUT_POST, "terms", FILTER_VALIDATE_BOOLEAN);
+
+if ( ! $terms) {
+    die("Terms must be accepted");
+}
+
+var_dump($symptoms, $diabetes, $hypertension, $asthma, $terms);
