5d92da8693ec7787469da6474d949f12c19d9b61
vishnudattaj <vishnudattaj@gmail.com>
2025-02-05 03:50:00 +0000

    fixed flask

--- Flask_Login/app.py
+++ Flask_Login/app.py
@@ -3,13 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 import flask_login
 from werkzeug.security import generate_password_hash, check_password_hash
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['RECAPTCHA_S8ITE_KEY'] = '6LcYcEohAAAAANVL5nwJ25oOM488BPaC9bujC-94'
+app.secret_key = '6LcYcEohAAAAAJ5JeDLnVKReHLj0ZIkeo7FgilZB'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db'
+db = SQLAlchemy(app)
+
 login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
 
 class LoginScreen(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     usernames = db.Column(db.String(100), unique=True, nullable=False)
     passwords = db.Column(db.String(200), nullable=False)
+
 
 class MedicalInfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,113 +28,102 @@ class MedicalInfo(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('login_screen.id'), nullable=False)
     user = db.relationship('LoginScreen', backref=db.backref('medical_info', lazy=True))
 
+
 class User(flask_login.UserMixin):
     pass
 
+
 @login_manager.user_loader
-def load_user(user_id):
-    user_entry = LoginScreen.query.filter_by(usernames=user_id).first()
-    if user_entry:
-        user = User()
-        user.id = user_entry.usernames
-        return user
-    return None
+def user_loader(username):
+    if username not in username:
+        return
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    user = User()
+    user.id = username
+    return user
 
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'login'
+with app.app_context():
+    db.create_all()
+    db.session.commit()
 
-    with app.app_context():
-        db.create_all()
-        db.session.commit()
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user_entry = LoginScreen.query.filter_by(usernames=username).first()
 
-    @app.route('/', methods=['GET', 'POST'])
-    def login():
-        if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            user_entry = LoginScreen.query.filter_by(usernames=username).first()
-            
-            if user_entry and check_password_hash(user_entry.passwords, password):
-                user = User()
-                user.id = username
-                flask_login.login_user(user)
-                return redirect(url_for('protected'))
-            return render_template('wrongCredentials.html')
-        return render_template('login.html')
-
-    @app.route('/signup', methods=['GET', 'POST'])
-    def signup():
-        if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            confirm = request.form['confirmpassword']
-            
-            if password != confirm:
-                return render_template('confirmPassword.html')
-                
-            if LoginScreen.query.filter_by(usernames=username).first():
-                return render_template('existingUser.html')
-                
-            hashed_pw = generate_password_hash(password)
-            new_user = LoginScreen(usernames=username, passwords=hashed_pw)
-            
-            db.session.add(new_user)
-            db.session.commit()
-            
+        if user_entry and check_password_hash(user_entry.passwords, password):
             user = User()
             user.id = username
             flask_login.login_user(user)
             return redirect(url_for('protected'))
-            
-        return render_template('signup.html')
+        return render_template('wrongCredentials.html')
+    return render_template('login.html')
 
-    @app.route('/protected', methods=['GET', 'POST'])
-    @flask_login.login_required
-    def protected():
-        if request.method == 'POST':
-            action = request.form.get('action')
-            
-            if action == "symptomSubmit":
-                if not request.form.get('terms'):
-                    return "Terms must be accepted", 400
-                    
-                current_user = LoginScreen.query.filter_by(
-                    usernames=flask_login.current_user.id
-                ).first()
-                
-                medical_entry = MedicalInfo(
-                    symptoms=request.form.get('symptoms'),
-                    diabetes='diabetes' in request.form,
-                    hypertension='hypertension' in request.form,
-                    asthma='asthma' in request.form,
-                    terms_accepted=True,
-                    user_id=current_user.id
-                )
-                
-                db.session.add(medical_entry)
-                db.session.commit()
-                
-                return render_template('homepage.html', save=current_user.usernames)
-                
-            elif action == "logOut":
-                return redirect(url_for('logout'))
-        
-        return render_template('homepage.html', save=flask_login.current_user.id)
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm = request.form['confirmpassword']
 
-    @app.route('/logout')
-    def logout():
-        flask_login.logout_user()
-        return 'Logged out'
+        if password != confirm:
+            return render_template('confirmPassword.html')
 
-    return app
+        if LoginScreen.query.filter_by(usernames=username).first():
+            return render_template('existingUser.html')
+
+        hashed_pw = generate_password_hash(password)
+        new_user = LoginScreen(usernames=username, passwords=hashed_pw)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        user = User()
+        user.id = username
+        flask_login.login_user(user)
+        return redirect(url_for('protected'))
+
+    return render_template('signup.html')
+
+@app.route('/protected', methods=['GET', 'POST'])
+@flask_login.login_required
+def protected():
+    if request.method == 'POST':
+        action = request.form.get('action')
+
+        if action == "symptomSubmit":
+            if not request.form.get('terms'):
+                return "Terms must be accepted", 400
+
+            current_user = LoginScreen.query.filter_by(
+                usernames=flask_login.current_user.id
+            ).first()
+
+            medical_entry = MedicalInfo(
+                symptoms=request.form.get('symptoms'),
+                diabetes='diabetes' in request.form,
+                hypertension='hypertension' in request.form,
+                asthma='asthma' in request.form,
+                terms_accepted=True,
+                user_id=current_user.id
+            )
+
+            db.session.add(medical_entry)
+            db.session.commit()
+
+            return render_template('homepage.html', save=flask_login.current_user.id)
+
+        elif action == "logOut":
+            return redirect(url_for('logout'))
+    return render_template('homepage.html', save=flask_login.current_user.id)
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True, port=5001)  # Explicitly set port
